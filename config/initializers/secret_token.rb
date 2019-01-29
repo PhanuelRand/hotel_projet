12aa69ad952e9b2754891cb79f83e5e04bec7137
PhanuelRand <numbermanstudio@gmail.com>
2019-01-29 10:28:41 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HotelProjet::Application.config.secret_token = '8f07a95fde07c5419e0c4212ebe9a499fccbffccefb1105ed3782673d1c8adccf872ad3bbc6b1521e19f0188de6d875e082d4aea7236c4fdac616ea8e6a17b62'
